6a38c3e3f9fcf074cd297f207fa81da7dea67096
Jacob Errington <jerrington@drw.com>
2021-06-29 00:19:56 -0400

    refactor directories

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./src/App";
-import Main from "./src/main";
-import reportWebVitals from "./src/reportWebVitals";
+import App from "./App";
+import Main from "./main";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
